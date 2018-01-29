f3a1a1f81e88308af8c722597f080eca48076266
Bruno G. Souza <bruno@bgsouza.com>
2018-01-29 10:10:50 -0200

    Update puppeteer.ts
    complement base v0.1

--- puppeteer.ts
+++ puppeteer.ts
@@ -1,8 +1,9 @@
 import { Promise, EventEmitter } from 'puppeteer';
+import { timeout, TimeoutError } from 'promise-timeout';
 const puppeteer = require('puppeteer');
 const devices = require('puppeteer/DeviceDescriptors');
 
-export class PuppeteerChrome {
+export class RAPuppeteerChrome {
     public browser : Promise;
     public page : EventEmitter;
 
@@ -10,24 +11,34 @@ export class PuppeteerChrome {
     }
 
     async initializeBrowser() {
-        this.browser = await puppeteer.launch({
-            headless: false
-        });
+        this.browser = await puppeteer.launch({ headless: true, ignoreHTTPSErrors: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
         this.page = await this.browser.newPage();
+        this.page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36')
         this.page.setViewport({
             width: 1920,
             height: 1080
-        });    
+        });   
     }
 
     async navigateTo(url) {
         if (!url)
           throw new Error('Missing URL');
-        return await this.page.goto(url);
+          return await this.page.goto(url, {
+            waitUntil: 'networkidle',
+            timeout: 180000
+        });
     }
 
     async takeScreenshot() {  
-        let image = await this.page.screenshot({fullPage: true, type: 'jpeg'});
+        let image = this.page.screenshot({fullPage: true, type: 'jpeg'});
+        await timeout(image, 30000)
+        .then((thing) => console.log("Screenshot successfull!"))
+        .catch((err) => {
+            //logger.verbose(err);
+            console.log(`[takeScreenshot][ERROR]: ${err}`)
+            image = null;
+            throw new Error(err);
+        });
         return image;
     }
 
@@ -40,54 +51,101 @@ export class PuppeteerChrome {
         return await this.browser.close();
     }
 
-    async executeScript(jQueryExpression, page?: EventEmitter) {
-        let p = page || this.page;
+    async pages() {
+        return await this.browser.pages();
+    }
+
+    async executeScript(jQueryExpression) {
         try {
-          const scriptResult = await p.evaluate(jQueryExpression);
+          const scriptResult = await this.page.evaluate(jQueryExpression);
           return scriptResult;
         } catch (err) {
-          console.error(err);
-          return {};
+          //console.error(err);
+          throw new Error(err);
+          //return {};
         }
     }
 
     async waitPageLoad() {
-        let pageready = false;
+        let pageready =  false;
         do {
         let readyState = await this.executeScript('document.readyState;');
-        pageready = readyState == 'complete';
+        pageready = readyState == 'complete' || readyState == 'interactive';
         } while (!pageready)
         return pageready;
     }
 
-    async openInNewTab(url: string) {
-        const page = await this.browser.newPage()
-        await page.goto(url);
+    async waitFor(ms: number) {
+        await this.page.waitFor(4000);
+    }
 
-        return page;
+    async wait(ms: number) {
+        await timeout(()=>{return true;}, 3000);
+    }
+
+    async openInNewTab(url: string) {
+        this.page = await this.browser.newPage()
     }
     
     async closeNewTab(page: EventEmitter) {
-        page.close();
+       await this.page.close();
+    }
 
-        return page;
+    async setCookie(cookie) {
+        let arrCookie = cookie.split('=');
+        let key = arrCookie[0];
+        let value = arrCookie[1];
+        return await this.page.setCookie({name:key, value:value});
+    }
+
+    async setCookieJQuery(cookie) {
+        await this.executeScript("document.cookie='"+cookie+"'");
+    }
+
+    async getCurrentUri() {
+        return await this.executeScript("location.href");
+    }
+    async getCultureInfo() {
+        let script = "navigator.language || navigator.userLanguage;";
+        return await this.executeScript(script);
     }
 
     async includeJQuery() {
         const jqueryscript = 
           `var s=window.document.createElement('script'); s.src='https://code.jquery.com/jquery-3.2.1.min.js'; window.document.head.appendChild(s);`;
+        console.log('Injetando JQuery');
         await this.executeScript(jqueryscript);
+        console.log('JQuery injetou');
       }
     
-      async hasJQuery() {
+    async hasJQuery() {
         let jqueryReady = false;
+        let hit = 0;
         let script = `document.querySelector('script[src="https://code.jquery.com/jquery-3.2.1.min.js"]') != null;`;
+        
         do {
+          console.log('Testando se tem JQuery');
           let readyState = await this.executeScript(script);
           jqueryReady = readyState == true;
+          hit++;
+
+          if(hit > 3) {
+              await this.includeJQuery();
+          }
         } while (!jqueryReady)
+        await this.waitFor(2000);
+        await this.executeScript('$=jQuery');
         return jqueryReady;
-      }
+        //await this.driver.executeScript(`$ = typeof $ == undefined ? jQuery : $`);
+    }
+
+    async IncludeAndAwaitJquery(timeout?: number) {
+        if(timeout != null) {
+            await this.waitFor(timeout);
+        }
+        await this.includeJQuery()
+        return await this.hasJQuery();
+    }
 
     private convertExpressionToJqueryFunction(jQueryExpression) {
         //This method returns only a function of the jquery expression or false.

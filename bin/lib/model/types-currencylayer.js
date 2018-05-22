c4af0273253a985a7c9a2e535eb2640a45152e3b
Bruno Souza <bruno@bgsouza.com>
2018-05-22 20:31:15 -0300

    remove bin

--- bin/lib/model/types-currencylayer.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var CurrencyLayerType;
-(function (CurrencyLayerType) {
-    CurrencyLayerType[CurrencyLayerType["HISTORICAL"] = 1] = "HISTORICAL";
-    CurrencyLayerType[CurrencyLayerType["LIVE"] = 2] = "LIVE";
-})(CurrencyLayerType = exports.CurrencyLayerType || (exports.CurrencyLayerType = {}));
-//# sourceMappingURL=types-currencylayer.js.map
